5ac808d83f795907ed0d64d38be840d5674825e3
vipul <vipul.devs99@gmail.com>
2024-12-10 11:43:22 +0530

    first commit

---
+++ components/misc/ButtonOutline..js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ButtonOutline = ({ children }) => {
+  return (
+    <button className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-white-500 text-white-500 bg-red-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-[#000] hover:text-white-500 transition-all hover:shadow-red ">
+      {" "}
+      {children}
+    </button>
+  );
+};
+
+export default ButtonOutline;
